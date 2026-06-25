9a35947c62d93fb630a203d01ca96f16c864c578
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-06-25 14:00:17 -0400

    Initial Commit

---
+++ RecalllQ/RecalllQApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecalllQApp.swift
+//  RecalllQ
+//
+//  Created by Fozia on 2026-06-25.
+//
+
+import SwiftUI
+
+@main
+struct RecalllQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
